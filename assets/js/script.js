76501c8e7e7ec77301703cf7660e3f23ff8d168a
wkedarprdxn <kedarw@prdxn.com>
2020-10-27 17:55:29 +0530

    js file check(need fix)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,9 @@
 /* Author: 
 
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function() {
+    $("form").submit(function(event) {
+        console.log($(this).serializeArray());
+        event.preventDefault();
+    });
+});
